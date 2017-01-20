df2794f0a0dfeb93a692de2a394d4a5531bebc84
Triwith <triwith.m@ku.th>
2017-01-20 17:46:44 +0700

    asdf

---
+++ src/Game/GameConsole.java
@@ -0,0 +1,39 @@
+package Game;
+import java.util.Scanner;
+import java.util.Random;
+public class GameConsole {
+	/** called object GuessingGame and use it. 
+	 * 
+	 * @param game called the object GuessingGame.
+	 * @return the correct number.
+	 */
+	public int play(GuessingGame game){
+		
+		game.getRandomNumber(20);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("I'm thinking of a number between 1 and "+game.upperBond);
+		boolean condit=true;
+		do{
+			System.out.print("What is your guess? :");
+			int input = sc.nextInt();
+			game.setHint(input);
+			if(game.guess(input)){
+				condit = false;
+				break;
+			}
+			else{
+				System.out.println(game.hint);
+			}
+			
+		game.setCount();
+			
+			
+		}while(condit);
+		System.out.println("Right! The secret is "+game.secret);
+		System.out.println("You used "+game.getCount()+" guesses.");
+		String input = sc.next();
+		return game.secret;
+		
+	}
+
+}
---
+++ src/Game/GuessingGame.java
@@ -0,0 +1,85 @@
+package Game;
+import java.util.Random;
+/** 
+ * Game of guessing a secret number.
+ * @author Triwith Mutitakul             
+ */
+
+public class GuessingGame {
+	/* properties of a guessing game */
+	public int upperBond;
+	public int secret;
+	public String hint;
+	public int count = 1;
+	/**
+	 * Initialize a new game.
+	 * @param upperbound is the max value for the secret number (>1).
+	 */
+
+	public GuessingGame(int upperBond){// this is a constructor
+		this.upperBond=upperBond;
+	}
+	
+	/** 
+	 * Create a random number between 1 and upperBond.
+	 * @param upperBond is the upper limit for random number
+	 * @return a random number between 1 and upperBond (inclusive)
+	 */
+	public int getRandomNumber(int upperBond){
+		long seed = System.currentTimeMillis( );
+		Random rand = new Random( seed );
+		
+		return this.secret = (rand.nextInt(upperBond) + 1);
+
+	}
+	/**check the input number. Correct or not.
+	 * 
+	 * @param number is a number that player guess.
+	 * @return the number that player guess is true or false.
+	 */
+	public boolean guess(int number){
+		if(number==secret){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	/**check the answer and choose the hint for player.
+	 * 
+	 * @param number is a number that player guess.
+	 */
+	public void setHint(int number){
+		if(number>secret){
+			 hint = "Sorry, you're guess is too large.";
+		}
+		else {
+			hint =  "Sorry, you're guess is too small.";
+		}
+	}
+	/** 
+	 * Return a hint based on the most recent guess.
+	 * @return hint based on most recent guess
+	 */
+
+	public String getHint(){
+		return this.hint;
+		
+	}
+	/**return number of guess that player used.
+	 * 
+	 * @return number of guess that player used.
+	 */
+	public int getCount(){
+		return this.count;
+	}
+	/**count the number of guess that player guess.
+	 * 
+	 */
+	public void setCount(){
+		count = this.count+1;
+	} 
+	
+
+
+}
